9c1f328e01177cc17e482854de31bf1b712a63a4
Gwen <gfernandez@wesleyan.edu>
2017-10-18 23:15:50 -0400

    meta fix

--- ratemds/spiders/md_spider.py
+++ ratemds/spiders/md_spider.py
@@ -12,9 +12,10 @@ class MDSpider(Spider):
 		doc_links = ['https://www.ratemds.com' + str(doc) for doc in doc_list]
 
 		for link in doc_links:
-			yield scrapy.Request(link, callback=self.parse_docs, meta={'link':link})
+			yield scrapy.Request(link, callback=self.parse_docs, meta={'doc_links':doc_links})
 
 	def parse_docs(self, response):
+		links = response.meta['doc_links']
 		doc_name = response.xpath('//div[@class="col-sm-6"]/h1/text()').extract_first()
 		specialty = response.xpath('//div[@class="search-item-info"]/a/text()').extract_first()
 		gender = response.xpath('//div[@class="col-sm-3 col-md-4 search-item-extra"]/div[1]//a/text()').extract_first()
